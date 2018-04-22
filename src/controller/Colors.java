2a0c2688fcb2416775059d94ff540a2386e6e407
Federica <federica.comuni@gmail.com>
2018-04-22 20:28:01 +0200

    initial commit

---
+++ src/controller/Colors.java
@@ -0,0 +1,18 @@
+package controller;
+
+/**
+ * Created by Federica on 06/07/2017.
+ */
+public class Colors {
+
+    private static String[] colors;
+
+    public Colors() {
+
+        colors = new String[] {"Yellow", "Orange", "Green", "Blue", "Violet", "Red", "Black", "Silver"};
+    }
+
+    public String[] getColors() {
+        return colors;
+    }
+}
---
+++ src/controller/Sequence.java
@@ -0,0 +1,43 @@
+package controller;
+
+
+import java.util.Random;
+
+/**
+ * Created by Federica on 06/07/2017.
+ */
+public class Sequence {
+
+    private String[] seqOfColors;
+    private Random rnd = new Random();
+    private boolean contains = false;
+    private Colors colors;
+
+    public Sequence() {
+
+        seqOfColors = new String[4];
+        colors = new Colors();
+
+        int counter = 0;
+
+        while (counter < 4) {
+            contains = false;
+            int random = rnd.nextInt(8);
+            for (String s : seqOfColors) {
+                if (s != null && s.equals(colors.getColors()[random])) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (!contains) {
+                seqOfColors[counter] = colors.getColors()[random];
+                counter++;
+            }
+        }
+
+    }
+
+    public String[] getSeqOfColors() {
+        return seqOfColors;
+    }
+}
